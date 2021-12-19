fcd44439ed12d93417594b725883aaa28821e76f
njulies <nj_july83@msn.com>
2021-12-19 21:02:49 +0700

    Exercise4
    Exercise4

---
+++ Exercise4_Noppadon_no.py
@@ -0,0 +1,10 @@
+fe=80.0     #Foundation English
+gb=79.5     #General Business
+ics=99.99   #Introduction to Computer Systems
+cp=60.0     #Computer Programming
+print("--- Your Score ---")
+print("Foundation English               : คะแนนที่ได้ = ", fe)
+print("General Business                 : คะแนนที่ได้ = ", gb)
+print("Introduction to Computer Systems : คะแนนที่ได้ = " , ics)
+print("Computer Programming             : คะแนนที่ได้ = ", cp)
+print("--- Congrachulation ---")
