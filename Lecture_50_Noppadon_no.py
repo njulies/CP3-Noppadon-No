72ec993b8b31cbd72370948980b049e7b970e812
njulies <nj_july83@msn.com>
2021-12-21 11:57:12 +0700

    lec50

---
+++ Lecture_50_Noppadon_no.py
@@ -0,0 +1,20 @@
+def num1(x, y):
+    print(x + y)
+def num2(x, y):
+    print(x - y)
+def num3(x, y):
+    print(x * y)
+def num4(x, y):
+    print(x / y)
+def num5(x, y):
+    print(x + y)
+    print(x - y)
+    print(x * y)
+    print(x / y)
+x=int(input("x :"))
+y=int(input("y :"))
+num1(x,y)
+num2(x,y)
+num3(x,y)
+num4(x,y)
+num5(x,y)
